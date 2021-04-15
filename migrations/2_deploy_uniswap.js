b103d71857374fa0879555f3a08c6bad1011dac3
Hayden Young <hayden.y@minertoken.io>
2021-04-16 02:55:18 +0800

    test: env vars.

--- migrations/2_deploy_uniswap.js
+++ migrations/2_deploy_uniswap.js
@@ -1,6 +1,7 @@
 const UniTokenSwap = artifacts.require("./UniTokenSwap");
 
 module.exports = async function(deployer, network) {
+    console.log("issuance address", process.env.ISSUANCE);
     const issuanceAddress = process.env.ISSUANCE;
     const uniswapFactoryAddress = process.env.UNISWAP_FACTORY;
     const minerAddress = process.env.MINER;
